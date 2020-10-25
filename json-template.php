2d9758472102c3d09ce25ef99db8b549516a0676
shariq619 <muhammadshariqali@gmail.com>
2020-10-24 21:03:49 -0700

    add http

--- json-template.php
+++ json-template.php
@@ -18,14 +18,19 @@ Template Name: Json Template
 		'taxonomy'                 => 'dealstore',
 		'pad_counts'               => false 
 	); 
-
 	$categories = get_categories( $args );
 	$stores = [];
 	foreach( $categories as $category ) {		
 		$stores['coupons'] = get_category_link( $category->term_id );
-		$stores['domain']  = get_field('domain_url', $category->taxonomy . '_' . $category->term_id);
+		$domain_url = get_field('domain_url', $category->taxonomy . '_' . $category->term_id);
+		if($domain_url){
+			$stores['domain']  = 'http://'.$domain_url;
+		} else {
+			$stores['domain']  = '';
+		}			
 		$super[] = $stores;	
-	}	
+	}
+	
 	$data = array(
 		'stores' => $super
 	);
